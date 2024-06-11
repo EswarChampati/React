803ddc0560cc767b8a62fcbec51d87d85c8c8012
eswar champati <eswarraghunadhavarma@gmail.com>
2024-06-11 16:33:04 +0530

    part5 of episode1

---
+++ Episode1/part5/App.js
@@ -0,0 +1,12 @@
+const parent =React.createElement(
+                     "div",
+                     {id:"parent"},
+                     [React.createElement("div",{id:"child1"},
+                        [React.createElement("h1",{},"i am h1 tag"),
+                        React.createElement("h2",{},"i am h2 tag")]),
+                     React.createElement("div",{id:"child2"},
+                        [React.createElement("h1",{},"i am h1 tag"),
+                        React.createElement("h2",{},"i am h2 tag")])]
+                     )
+const root=ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent)
