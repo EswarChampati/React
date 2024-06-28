f35d01c78ee0f2c5d6f43e9ebe00aa799c88ed6d
eswar champati <eswarraghunadhavarma@gmail.com>
2024-06-28 15:59:02 +0530

    lifiting the stateing up and controlled components

--- Episode11/src/components/Body.jsx
+++ Episode11/src/components/Body.jsx
@@ -15,7 +15,7 @@ const Body = () => {
     fetchData();
   }, []);
 
-  console.log(listOfRestaurants);
+  //console.log(listOfRestaurants);
   const fetchData = async () => {
     const data = await fetch(
       "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
--- Episode11/src/components/RestaurentMenu.jsx
+++ Episode11/src/components/RestaurentMenu.jsx
@@ -1,36 +1,52 @@
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
 import useRestaurentMenu from "../utils/useRestaurentMenu";
+import RestaurentCategory from "./RestaurentCategory";
+import { useState } from "react";
 
 const RestaurentMenu = () => {
+  const [showIndex, setShowIndex] = useState(0);
+
   const { resId } = useParams(); //resId name should be matched with the dynamic route name
 
   const resInfo = useRestaurentMenu(resId);
 
   if (resInfo === null) return <Shimmer />;
+  //console.log(resInfo);
 
   const restaurantInfo = resInfo?.data?.cards[2]?.card?.card?.info;
   const itemCards =
     resInfo?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card
       ?.card?.itemCards;
+  // console.log(
+  //   resInfo?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards
+  // );
+  const section =
+    resInfo?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+      (obj) => {
+        return (
+          obj.card?.card?.["@type"] ===
+          "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+        );
+      }
+    );
+  // console.log(section);
 
   const { name, cuisines, costForTwoMessage, avgRating, sla } =
     restaurantInfo || {};
 
   return (
-    <div className="menu">
-      <h1>{name}</h1>
-      <h2>{cuisines?.join(",")}</h2>
-      <h3>{costForTwoMessage}</h3>
-      <h3>{"Rating: " + avgRating}</h3>
-      <h3>{"Delivery time: " + sla?.deliveryTime}</h3>
-      <ul>
-        {itemCards?.map((foodItem) => (
-          <li key={foodItem?.card?.info?.id}>
-            {foodItem?.card?.info?.name}--{foodItem?.card?.info?.price / 100}
-          </li>
-        ))}
-      </ul>
+    <div className="text-center">
+      <h1 className="text-2xl font-bold my-3">{name}</h1>
+      <h2 className="text-lg font-bold">{cuisines?.join(",")}</h2>
+      {section.map((category, index) => (
+        <RestaurentCategory
+          key={index} /*index is used because i dont where id is there */
+          data={category?.card?.card}
+          showItems={showIndex === index}
+          setShowIndex={() => setShowIndex(index)}
+        />
+      ))}
     </div>
   );
 };
